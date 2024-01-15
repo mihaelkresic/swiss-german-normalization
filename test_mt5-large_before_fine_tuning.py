5e356648a3d120d58a7e00395c0028b8c8b7e1e8
mihaelkresic <153688645+mihaelkresic@users.noreply.github.com>
2024-01-15 23:57:30 +0100

    Create test_mt5-large_before_fine_tuning.py

---
+++ test_mt5-large_before_fine_tuning.py
@@ -0,0 +1,68 @@
+# Import necessary modules
+import pandas as pd
+import numpy as np
+
+import torch
+
+import logging
+from simpletransformers.t5 import T5Model, T5Args
+
+import sacrebleu
+
+# Now, import your refactored data_processing module
+import data_processing
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def perform_inference(test_df, model):
+    # Perform the inference and add a new column with predictions to the DataFrame
+    test_df["preds"] = model.predict(test_df.input_text.values.tolist())
+    return test_df
+
+def main(model, json_file_path):
+    # Use the refactored data_processing module to get the test data
+    _, _, test_df = data_processing.get_data_splits(json_file_path)
+
+    # Perform inference
+    result_df = perform_inference(test_df, model)
+    return result_df
+
+if __name__ == "__main__":
+    # Define model arguments
+    model_args = T5Args()
+    model_args.max_length = 100
+    model_args.length_penalty = 2.5
+    model_args.repetition_penalty = 1.5
+    model_args.num_beams = 5
+
+    # Initialize the model
+    model = T5Model("mt5", "google/mt5-large", args=model_args)
+
+    # Path to your JSON file containing the test data
+    json_file_path = '/content/swiss-german-normalization/sentences_ch_de_numerics.json'
+
+    # Call the main function to perform inference
+    result_df = main(model, json_file_path)
+
+    # Prepare the daata for ChrF++ calculation and calculate in on a sentence by sentence case
+    # Convert the 'preds' column to list of strings and the 'target_text' column to list of lists of strings
+    chrf_scores = []
+    for index, row in result_df.iterrows():
+        prediction = row['preds']
+        reference = [row['target_text']]
+        score = sacrebleu.sentence_chrf(prediction, reference, beta=2, word_order=1).score
+        chrf_scores.append(score)
+
+    scores = sum(chrf_scores) / len(chrf_scores)
+    
+    print('')
+    print('ChrF++ score: ') 
+    print(scores) 
+    print('')
+
+    # Optional: Save the result_df to a CSV file
+    output_folder = '/content/drive/MyDrive/swiss-german-normalization/'
+    result_df.to_csv(output_folder + 'test_predictions_mt5-large_before_finetuning.csv', index=False)
+    logger.info("Inference completed and saved to CSV.")
