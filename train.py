31d52e210c660619fa168f0eb4cc95174cfe2603
mihaelkresic <153688645+mihaelkresic@users.noreply.github.com>
2024-01-31 09:20:21 +0100

    Update train.py

--- train.py
+++ train.py
@@ -100,8 +100,11 @@ def main(model_size):
     # Train the model
     trainer.train()
 
-    best_model_dir = f"/content/drive/MyDrive/swiss-german-normalization/{model_size}/best_model/"
+    best_model_dir = f"/content/drive/MyDrive/swiss-german-normalization/{model_size}/best_model_/"
     model.save_pretrained(best_model_dir)
+
+    best_model_dir2 = f"/content/drive/MyDrive/swiss-german-normalization/{model_size}/best_model/"
+    trainer.save_model(best_model_dir2)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train mT5 model')
